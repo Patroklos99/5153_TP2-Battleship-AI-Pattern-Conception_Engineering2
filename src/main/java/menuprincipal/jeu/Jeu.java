574b147801e52ad52519d792aa73604728ec55c1
Philippe Bélanger <belanger.philippe.10@courrier.uqam.ca>
2022-11-24 20:54:36 -0500

    Ajout de outline pour Jeu

--- src/main/java/menuprincipal/jeu/Jeu.java
+++ src/main/java/menuprincipal/jeu/Jeu.java
@@ -2,12 +2,20 @@ package menuprincipal.jeu;
 
 import lombok.Data;
 import javafx.util.Pair;
+import menuprincipal.battleship.joueur.Joueur;
 import menuprincipal.battleship.plateau.Plateau;
+import menuprincipal.battleship.plateau.PlateauBateau;
+import menuprincipal.battleship.plateau.PlateauTir;
 
 @Data
 public class Jeu {
 
     private static Jeu instanceJeu = null;
+
+    private Joueur joueur1, joueur2;
+    private PlateauBateau plateauBateau_joueur1, plateauBateau_joueur2;
+    private PlateauTir plateauTir_joueur1, plateauTir_joueur2;
+
 
     private Jeu() {}
 
@@ -19,20 +27,42 @@ public class Jeu {
     }
 
     public void jouer() {
+        determinerModeJeu();
+        initialiserPlateaux();
+        placerBateaux();
 
+        Joueur gagnant = null;
+        while(gagnant == null){
+            effectuerProchaintour();
+            gagnant = determinerGagnant();
+        }
+        
     }
+
+
 
     private void determinerModeJeu() {
-        //
+        //TODO: Initialiser les joueurs basé sur le choix de l'utilisateur.
     }
 
-    private void actualiserPlateau(Pair<Double, Double> pair, Plateau plateau) {
+    private void initialiserPlateaux() {
+        //TODO: Initialiser plateaux vides.
+    }
+    private void placerBateaux() {
+        //TODO: Demander aux joueurs de placer tout les bateaux.
+    }
 
+    private void actualiserPlateau(Pair<Integer, Integer> pair, PlateauTir plateau) {
+        //TODO: actualiser la case d'un plateau, avec le tir choisi par un joueur.
     }
 
     private void effectuerProchaintour() {
-
+        //TODO: Demander aux deux joueurs de choisir une case où tirer.
     }
 
-    private void determinerGagnant() {}
+    private Joueur determinerGagnant() {
+        //TODO: Faire un check pour voir si un joueur est gagnant
+        //Retourner le joueur gagnant, sinon, retourne null.
+        return null;
+    }
 }
