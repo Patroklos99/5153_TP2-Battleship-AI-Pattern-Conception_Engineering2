8e8bb4cc604c01c8edd4943fd2c1cbd19b81fb8f
Lysanne Chagnon <gk591048@ens.uqam.ca>
2022-12-01 19:59:44 -0500

    ajout de la vérif pour les entrées (porte-avion seulement)

--- src/main/java/menuprincipal/battleship/joueur/Personne.java
+++ src/main/java/menuprincipal/battleship/joueur/Personne.java
@@ -17,7 +17,26 @@ public class Personne extends Joueur{
 
         System.out.println("Veuillez entrer les coordonnées du porte-avion qui utilise 5 cases (exemple: A1 A2 A3 A4 A5):");
         coordonnee = entreePersonne.nextLine();
+        coordonnee.toLowerCase();
 
+        String[] plusieursCoordonnees = coordonnee.split(" ");
+        Boolean entreeCorrecte = true;
+        if(plusieursCoordonnees.length != 5)
+            entreeCorrecte = false;
+        for(String coor: plusieursCoordonnees){
+            if(!Character.isAlphabetic(coor.charAt(0)) || !Character.isDigit(coor.charAt(1)) || (coor.length() == 3 && !Character.isDigit(coor.charAt(2)))
+            || (coor.length() != 2 && coor.length() != 3) || (coor.length() == 3 && !(coor.charAt(1) == '1' && coor.charAt(2) == '0'))){
+                entreeCorrecte = false;
+                break;
+            }
+        }
+
+
+
+
+
+
+        /**
         System.out.println("Veuillez entrer les coordonnées du croiseur qui utilise 4 cases (exemple: B1 B2 B3 B4):");
         coordonnee = entreePersonne.nextLine();
 
@@ -29,5 +48,6 @@ public class Personne extends Joueur{
 
         System.out.println("Veuillez entrer les coordonnées du torpilleur qui utilise 2 cases (exemple: E1 E2):");
         coordonnee = entreePersonne.nextLine();
+         */
     }
 }
