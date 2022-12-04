9625df9d3d32e861f132b7fbc5184ffe70d1f36c
Lysanne Chagnon <gk591048@ens.uqam.ca>
2022-12-04 14:17:38 -0500

    fix d'un bug qui permettait à l'utilisateur de mettre 0 lors du placement de bateau

--- src/main/java/menuprincipal/battleship/joueur/Personne.java
+++ src/main/java/menuprincipal/battleship/joueur/Personne.java
@@ -58,7 +58,7 @@ public class Personne extends Joueur{
         for(String coor: plusieursCoordonnees){
             if(!Character.isAlphabetic(coor.charAt(0)) || !Character.isDigit(coor.charAt(1)) || (coor.length() == 3 && !Character.isDigit(coor.charAt(2)))
                     || (coor.length() != 2 && coor.length() != 3) || (coor.length() == 3 && !(coor.charAt(1) == '1' && coor.charAt(2) == '0'))
-                    || (coor.charAt(0) < 'a' && coor.charAt(0) > 'j' )){
+                    || (coor.charAt(0) < 'a' && coor.charAt(0) > 'j' ) || (coor.length() == 2 && coor.charAt(1) == '0')){
                 estCorrect = false;
                 break;
             }
