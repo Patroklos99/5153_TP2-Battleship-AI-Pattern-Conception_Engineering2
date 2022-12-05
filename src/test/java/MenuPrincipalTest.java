d1000115786ffc753e27885a3325eed08caf7265
Philippe Bélanger <belanger.philippe.10@courrier.uqam.ca>
2022-12-04 22:50:24 -0500

    Update de pom.xml pour effectuer les tests sur maven 3.8.6

--- src/test/java/MenuPrincipalTest.java
+++ src/test/java/MenuPrincipalTest.java
@@ -1,8 +1,8 @@
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MenuPrincipalTest {
 
-    @Test
+    @org.junit.jupiter.api.Test
     void main() {
     }
 }
