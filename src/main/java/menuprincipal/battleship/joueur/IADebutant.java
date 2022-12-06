0f2243d9c4b2cb615d4d74916185cbbee5d0ec47
Lysanne Chagnon <gk591048@ens.uqam.ca>
2022-12-06 15:27:17 -0500

    ajout de determinerTir pour IAdebutant

--- src/main/java/menuprincipal/battleship/joueur/IADebutant.java
+++ src/main/java/menuprincipal/battleship/joueur/IADebutant.java
@@ -1,4 +1,19 @@
 package menuprincipal.battleship.joueur;
 
+import javafx.util.Pair;
+import menuprincipal.battleship.plateau.Coordonnee;
+
+import java.util.Random;
+
 public class IADebutant extends Joueur{
+
+    @Override
+    protected Coordonnee determinerTir() {
+        Random randomNum = new Random();
+        int colonne = randomNum.nextInt(9 + 1);
+        int rangee = randomNum.nextInt(9 + 1);
+        return new Coordonnee(colonne,rangee);
+    }
+
+
 }
